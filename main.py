89226bb060e2a889786cf6d4a7782ade16a9492c
Jessika Milhomem <jessika.milhomem@gmail.com>
2020-01-01 16:40:03 -0300

    Included a menu

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-"""Desafio Rota Application."""
+"""Route Challenge Application."""
 import sys
-from utils.files_manipulation import *
-from cheaper_flight.cheaper_flight import *
+from utils.files_manipulation import import_file
+from cheaper_flight.cheaper_flight import return_source_list
+from cheaper_flight.cheaper_flight import return_destination_list
+from cheaper_flight.cheaper_flight import return_prefinal_routes
+from cheaper_flight.cheaper_flight import return_cheapest_route
 
 
-def main(p_argv):
-    # Import file
-    file = import_file(sys.argv[1])
+def main(filename):
+    """Main function to call the program."""
+    stop_defined = ""
+    while stop_defined.lower() != "yes":
+        execute_app(filename)
+        stop_defined = input("\nDO YOU WANT TO QUIT? (YES/NO): ")
+
+
+def execute_app(filename):
+    """Function to execute the program."""
+    file = import_file(filename)
 
     # Define the initial and the last destinations
     travel = str(input("PLEASE, ENTER DE ROUTE: "))
@@ -15,52 +26,52 @@ def main(p_argv):
     sour_list = return_source_list(sour_airport, file)
     dest_list = return_destination_list(dest_airport, sour_list, file)
 
-    # Define the route possibilities.
-    route_list = []
-    for ini_middle, end_middle, value in dest_list:
-        route_list = return_destination_list(ini_middle, sour_list, file)
+    try:
+        # Define the route possibilities.
+        route_list = []
+        for ini_middle, end_middle, value in dest_list:
+            route_list = return_destination_list(ini_middle, sour_list, file)
 
-    # Create the intermediate route.
-    prefinal_routes = []
-    prefinal_routes = dest_list + route_list
+        # Create the intermediate route.
+        prefinal_routes = []
+        prefinal_routes = dest_list + route_list
 
-    middle_list = return_route_list(route_list,
-                                    file,
-                                    sour_list,
-                                    prefinal_routes)
+        # Define the final list of routes possibilities
+        final_list = []
+        aux_list = []
+        for ini in sour_list:
 
-    # Define the final list of routes possibilities
-    final_list = []
-    aux_list = []
-    for ini in sour_list:
+            # Define one of final routes,if there is a direct flight
+            if ini[0] == sour_airport and ini[len(ini) - 2] == dest_airport:
+                final_list.append(ini)
 
-        # Define one of final routes,if there is a direct flight
-        if ini[0] == sour_airport and ini[len(ini) - 2] == dest_airport:
-            final_list.append(ini)
+            else:
+                if ini not in final_list:
+                    aux_list = return_prefinal_routes(ini,
+                                                      dest_airport,
+                                                      prefinal_routes)
 
-        else:
-            if ini not in final_list:
-                aux_list = return_prefinal_routes(ini,
-                                                  dest_airport,
-                                                  prefinal_routes)
+                if aux_list:
+                    final_list.append(aux_list)
 
-            if aux_list:
-                final_list.append(aux_list)
+        print("\nPOSSIBILITIES:")
+        for routes in final_list:
+            routes = str(routes)\
+                     .replace("[", "")\
+                     .replace("]", "")\
+                     .replace("'", "")\
+                     .replace(",", " - ")
+            print(routes, "$")
 
-    print("\nPOSSIBILITIES:")
-    for routes in final_list:
-        routes = str(routes)\
-                 .replace("[", "")\
-                 .replace("]", "")\
-                 .replace("'", "")\
-                 .replace(",", " - ")
-        print(routes, "$")
+        # Define and  print the cheapest flight ticket
+        result = return_cheapest_route(final_list)
+        print("\nBEST ROUTE:")
+        print("{} > ${}".format(result[0], result[1]))
 
-    # Define and  print the cheapest flight ticket
-    result = return_cheapest_route(final_list)
-    print(f"\nBEST ROUTE:")
-    print(f"{result[0]} > ${result[1]}")
+    except UnboundLocalError:
+        print("Please, check the route informed. Anyone not exist in file!")
 
 
 if __name__ == "__main__":
-    result = main(sys.argv)
+    filename = sys.argv[1]
+    main(filename)
